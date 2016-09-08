818341e04f2d263daf964a919c430a1f2032bdb1
Trey Tacon <ttacon@gmail.com>
2016-09-08 11:35:12 -0400

    Fix cleanup

--- build/build.sh
+++ build/build.sh
@@ -11,6 +11,7 @@ go get github.com/ttacon/deploy-go-example/...
 # Checkout the latest version to build for production
 git -C $GOPATH/src/github.com/ttacon/deploy-go-example checkout production
 # TODO: add dependency checkout example
-# Rebuild the package, we can add fun things like GOOS or GOARCH here
-go build -o deploy-go-example  github.com/ttacon/deploy-go-example
+# Rebuild the package
+GOOS=linux go build -o deploy-go-example  github.com/ttacon/deploy-go-example
 docker build -t deploy-go-example -f Dockerfile.dist .
+rm deploy-go-example
